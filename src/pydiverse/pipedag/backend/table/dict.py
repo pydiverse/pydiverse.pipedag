89c2e1dfcd2f1658ee75d4797f779874eb31cd46
Nicolas Camenisch <garnele007@gmail.com>
2022-07-27 17:56:45 +0200

    Implement TableHooks for DictTableStore

--- src/pydiverse/pipedag/backend/table/dict.py
+++ src/pydiverse/pipedag/backend/table/dict.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
 import threading
+import warnings
+
+import pandas as pd
 
 from pydiverse.pipedag._typing import T
 from pydiverse.pipedag.backend.metadata import TaskMetadata
-from pydiverse.pipedag.backend.table.base import BaseTableStore
+from pydiverse.pipedag.backend.table.base import BaseTableStore, TableHook
 from pydiverse.pipedag.core import MaterialisingTask, Schema, Table
 from pydiverse.pipedag.errors import CacheError, SchemaError
 
@@ -19,7 +22,7 @@ class DictTableStore(BaseTableStore):
         self.store = dict()
         self.metadata = dict()
         self.run_metadata = dict()
-        self.__lock = threading.Lock()
+        self.__lock = threading.RLock()
 
     def create_schema(self, schema: Schema):
         with self.__lock:
@@ -57,7 +60,9 @@ class DictTableStore(BaseTableStore):
         with self.__lock:
             if table.name in self.store[schema.working_name]:
                 raise Exception(f"Table with name '{table.name}' already in store.")
-            self.store[schema.working_name][table.name] = table.obj
+
+            hook = self.get_m_table_hook(type(table.obj))
+            hook.materialise(self, table, schema.working_name)
 
     def copy_table_to_working_schema(self, table: Table):
         schema = table.schema
@@ -76,17 +81,11 @@ class DictTableStore(BaseTableStore):
         self, table: Table[T], as_type: type[T], from_cache: bool = False
     ) -> T:
         with self.__lock:
-            if from_cache:
-                obj = self.store[table.schema.name][table.name]
-            else:
-                obj = self.store[table.schema.current_name][table.name]
+            schema = table.schema
+            schema_name = schema.name if from_cache else schema.current_name
 
-        if isinstance(obj, as_type):
-            return obj.copy()
-
-        raise TypeError(
-            f"{type(self).__name__} can't convert from type {type(obj)} to {as_type}."
-        )
+            hook = self.get_r_table_hook(as_type)
+            return hook.retrieve(self, table, schema_name, as_type)
 
     def store_task_metadata(self, metadata: TaskMetadata, schema: Schema):
         with self.__lock:
@@ -108,3 +107,56 @@ class DictTableStore(BaseTableStore):
                     "Failed to retrieve metadata for task "
                     f"'{task.name}' with cache key '{task.cache_key}'"
                 )
+
+
+@DictTableStore.register_table(pd)
+class PandasTableHook(TableHook[DictTableStore]):
+    @classmethod
+    def can_materialise(cls, type_) -> bool:
+        return issubclass(type_, pd.DataFrame)
+
+    @classmethod
+    def can_retrieve(cls, type_) -> bool:
+        return type_ == pd.DataFrame
+
+    @classmethod
+    def materialise(cls, store, table: Table[pd.DataFrame], schema_name):
+        store.store[schema_name][table.name] = table.obj
+
+    @classmethod
+    def retrieve(cls, store, table, schema_name, as_type):
+        return store.store[schema_name][table.name].copy()
+
+
+try:
+    import pydiverse.transform as pdt
+except ImportError as e:
+    warnings.warn(str(e), ImportWarning)
+    pdt = None
+
+
+@DictTableStore.register_table(pdt)
+class PydiverseTransformTableHook(TableHook[DictTableStore]):
+    @classmethod
+    def can_materialise(cls, type_) -> bool:
+        return issubclass(type_, pdt.Table)
+
+    @classmethod
+    def can_retrieve(cls, type_) -> bool:
+        from pydiverse.transform.eager import PandasTableImpl
+
+        return issubclass(type_, PandasTableImpl)
+
+    @classmethod
+    def materialise(cls, store, table: Table[pdt.Table], schema_name):
+        from pydiverse.transform.core.verbs import collect
+
+        table.obj = table.obj >> collect()
+        return PandasTableHook.materialise(store, table, schema_name)
+
+    @classmethod
+    def retrieve(cls, store, table, schema_name, as_type):
+        from pydiverse.transform.eager import PandasTableImpl
+
+        df = PandasTableHook.retrieve(store, table, schema_name, pd.DataFrame)
+        return pdt.Table(PandasTableImpl(table.name, df))
