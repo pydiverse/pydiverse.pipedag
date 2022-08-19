b5745117174f8faa978c6879b53bee43e491db49
Nicolas Camenisch <garnele007@gmail.com>
2022-08-19 10:22:32 +0200

    Fix incorrect .gitignore (excluded ALL __init__.py files)

---
+++ src/pydiverse/pipedag/engine/__init__.py
@@ -0,0 +1,9 @@
+from .base import Engine
+from .prefect import PrefectEngine, PrefectOneEngine, PrefectTwoEngine
+
+__all__ = [
+    "Engine",
+    "PrefectEngine",
+    "PrefectOneEngine",
+    "PrefectTwoEngine",
+]
