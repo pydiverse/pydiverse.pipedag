bd0eced386b1723458fbfa275b5c0231c6d782c7
windiana42 <windiana@users.sf.net>
2022-09-16 15:48:53 +0200

    More robust SELECT INTO injection for mssql

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,10 @@
+import logging
 import os
 
+import structlog
+
 os.environ["PIPEDAG_CONFIG"] = os.path.join(os.path.dirname(__file__), "pipedag.toml")
+
+structlog.configure(
+    wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+)
