da7d0875cd0a97d2056f2b771ee009c8b93866a2
windiana42 <windiana@users.sf.net>
2022-11-23 10:21:03 +0100

    Fixed INTO insertion and added test that was failing before

--- tests/test_sql_ddl.py
+++ tests/test_sql_ddl.py
@@ -7,6 +7,7 @@ def test_insert_into():
         "Select 1": "Select 1 INTO a.b.c",
         "Select 1 WHERE TRUE": "Select 1 INTO a.b.c WHERE TRUE",
         "Select 1 GROUP\nBY x": "Select 1 INTO a.b.c GROUP\nBY x",
+        "Select 1 FROM A GROUP BY x": "Select 1 INTO a.b.c FROM A GROUP BY x",
         "Select 1 UNION ALL SELECT 2": "Select 1 INTO a.b.c UNION ALL SELECT 2",
         "Select 1 From X": "Select 1 INTO a.b.c From X",
         "Select (SELECT 1 FROM Y) From X": "Select (SELECT 1 FROM Y) INTO a.b.c From X",
