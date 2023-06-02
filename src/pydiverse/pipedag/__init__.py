3a76be22e4ca260018f198ba43f6dee85b107c55
Nicolas Camenisch <garnele007@gmail.com>
2023-06-02 12:01:21 +0200

    Add ConfigContext to public interface

--- src/pydiverse/pipedag/__init__.py
+++ src/pydiverse/pipedag/__init__.py
@@ -1,8 +1,10 @@
+from pydiverse.pipedag.context import ConfigContext
 from pydiverse.pipedag.core import Flow, PipedagConfig, Stage, Task
 from pydiverse.pipedag.materialize import Blob, Table, materialize
 
 __all__ = [
     "Blob",
+    "ConfigContext",
     "Flow",
     "PipedagConfig",
     "Stage",
