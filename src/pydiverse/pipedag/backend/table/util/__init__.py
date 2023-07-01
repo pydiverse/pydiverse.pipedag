7137727976679bfd1de0e2dabb16810146985880
Nicolas Camenisch <garnele007@gmail.com>
2023-07-01 14:01:56 +0200

    Remove engine_dispatch

--- src/pydiverse/pipedag/backend/table/util/__init__.py
+++ src/pydiverse/pipedag/backend/table/util/__init__.py
@@ -1,8 +1,3 @@
 from __future__ import annotations
 
 from .dtype import DType, PandasDTypeBackend
-from .engine_dispatch import (
-    classmethod_engine_argument_dispatch,
-    engine_argument_dispatch,
-    engine_dispatch,
-)
