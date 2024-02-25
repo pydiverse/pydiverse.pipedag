e62637d3c8670cc218933788c622fa06129b8267
Nicolas Müller <github@nicolasmueller.eu>
2024-02-25 18:54:03 +0100

    Randomly assign query string for lazy task that does not provide a query string in order to ensure cache invalidity

--- src/pydiverse/pipedag/backend/table/base.py
+++ src/pydiverse/pipedag/backend/table/base.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import sys
+import uuid
 from abc import ABC, abstractmethod
 from collections.abc import Iterable
 from enum import Enum
@@ -337,18 +338,17 @@ class BaseTableStore(TableHookResolver, Disposable):
         try:
             hook = self.get_m_table_hook(type(table.obj))
             query_str = hook.lazy_query_str(self, table.obj)
-            query_hash = stable_hash("LAZY-TABLE", query_str)
         except TypeError:
-            # This table type doesn't provide a query string
-            # -> Fallback to default implementation
             self.logger.warning(
                 f"The output table {table.name} using given by"
                 f" {repr(table.obj)} of the lazy task {task.name} does"
                 " not provide a query string. Lazy evaluation is not"
                 " possible. Assuming that the task is not cache valid."
             )
-            TaskContext.get().is_cache_valid = False
-            return self.store_table(table, task)
+            # Assign random query string to ensure that task is not cache valid
+            query_str = uuid.uuid4().hex
+
+        query_hash = stable_hash("LAZY-TABLE", query_str)
 
         # Store the table
         try:
