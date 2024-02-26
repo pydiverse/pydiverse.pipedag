8cbee8918b5898bba7cd39972d23cc9061c470e5
Nicolas Müller <github@nicolasmueller.eu>
2024-02-26 12:23:25 +0100

    Improve test

--- tests/test_cache/test_basic_cache_invalidation.py
+++ tests/test_cache/test_basic_cache_invalidation.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import pandas as pd
+import polars as pl
 
 from pydiverse.pipedag import Blob, ConfigContext, Flow, Stage, Table
 from pydiverse.pipedag.context import StageLockContext
@@ -599,31 +600,69 @@ def test_ignore_task_version(mocker):
             child_spy.assert_called_once()
 
 
-def test_lazy_task_without_query_string():
-    value = 0
+def test_lazy_task_without_query_string(mocker):
+    value = None
 
-    @materialize(lazy=True)
+    @materialize(lazy=True, nout=5)
     def falsely_lazy_task():
-        return pd.DataFrame({"x": [value]})
-
-    @materialize(version="1.0", input_type=pd.DataFrame)
-    def take_first(table):
-        return table["x"][0]
+        return (
+            Table(pd.DataFrame({"x": [value]}), name="pd_table"),
+            Table(pl.DataFrame({"x": [value]}), name="pl_table"),
+            Table(pl.DataFrame({"x": [value]}).lazy(), name="pl_lazy_table"),
+            select_as(2, "y"),
+            3,
+        )
 
     def get_flow():
         with Flow() as flow:
             with Stage("stage_1"):
-                tbl = falsely_lazy_task()
-                res = take_first(tbl)
-        return flow, res
+                pd_tbl, pl_tbl, pl_lazy_tbl, select_tbl, constant = falsely_lazy_task()
+                res_pd = m.take_first(pd_tbl)
+                res_pl = m.take_first(pl_tbl)
+                res_pl_lazy = m.take_first(pl_lazy_tbl)
+                res_select = m.noop(select_tbl)
+                res_constant = m.noop(constant)
+        return flow, res_pd, res_pl, res_pl_lazy, res_select, res_constant
 
-    flow, res = get_flow()
+    value = 0
+    flow, res_pd, res_pl, res_pl_lazy, res_select, res_constant = get_flow()
     with StageLockContext():
         result = flow.run()
-        assert result.get(res) == 0
+        assert result.get(res_pd) == 0
+        assert result.get(res_pl) == 0
+        assert result.get(res_pl_lazy) == 0
 
     value = 1
-    flow, res = get_flow()
+    flow, res_pd, res_pl, res_pl_lazy, res_select, res_constant = get_flow()
+    res_pd_spy = spy_task(mocker, res_pd)
+    res_pl_spy = spy_task(mocker, res_pl)
+    res_pl_lazy_spy = spy_task(mocker, res_pl_lazy)
+    select_spy = spy_task(mocker, res_select)
+    constant_spy = spy_task(mocker, res_constant)
     with StageLockContext():
         result = flow.run()
-        assert result.get(res) == 1
+        assert result.get(res_pd) == 1
+        assert result.get(res_pl) == 1
+        assert result.get(res_pl_lazy) == 1
+        # res_pd is downstream of a pd.DataFrame from a lazy task,
+        # which should always be cache invalid. Hence, it should always be called.
+        res_pd_spy.assert_called_once()
+
+        # res_pd is downstream of a pl.DataFrame from a lazy task,
+        # which should always be cache invalid. Hence, it should always be called.
+        res_pl_spy.assert_called_once()
+
+        # res_pd is downstream of a pl.LazyFrame from a lazy task,
+        # which should always be cache invalid. Hence, it should always be called.
+        # To avoid cache-invalidating the LazyFrame, we should use AUTOVERSION.
+        res_pl_lazy_spy.assert_called_once()
+
+        # res_select is downstream of an SQL query `select_tbl` (which did not change)
+        # from a lazy task, hence select_tbl should be cache valid and the task
+        # producing res_select should not be called.
+        select_spy.assert_not_called()
+
+        # res_constant is downstream of the constant `constant` (which did not change)
+        # from a lazy task, hence res_constant should be cache valid and the task
+        # producing res_select should not be called.
+        constant_spy.assert_not_called()
