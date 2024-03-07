0420ab4f5e5a9b4eb343b36ed7f859c3ab8e9f88
windiana42 <windiana@users.sf.net>
2024-03-07 08:25:06 +0100

    Fix unicode test for ibm db2.

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -14,18 +14,20 @@ from tests.fixtures.instances import (
     with_instances,
 )
 from tests.util import tasks_library as m
+from tests.util.tasks_library import simple_dataframe
 
 pytestmark = [with_instances(ALL_INSTANCES, ORCHESTRATION_INSTANCES)]
 
 
-def test_unicode():
-    @materialize(lazy=True)
-    def unicode():
-        return sa.text("SELECT 'äöüßéç' as a")
+def test_unicode(unicode_str="äöüßéç"):
+    @materialize(lazy=True, input_type=sa.Table)
+    def unicode(src):
+        return sa.select(sa.literal(unicode_str).label("a")).select_from(src).limit(1)
 
     with Flow("flow") as f:
         with Stage("stage"):
-            x = unicode()
+            dummy_source = simple_dataframe()
+            x = unicode(dummy_source)
             x2 = m.noop(x)
             x3 = m.noop_lazy(x2)
             m.assert_table_equal(x, x2)
@@ -34,24 +36,9 @@ def test_unicode():
     with StageLockContext():
         result = f.run()
         assert result.successful
-        assert result.get(x3, as_type=pd.DataFrame)["a"][0] == "äöüßéç"
+        assert result.get(x3, as_type=pd.DataFrame)["a"][0] == unicode_str
 
 
 @skip_instances("mssql", "mssql_pytsql")
 def test_unicode_beyond_mssql():
-    @materialize(lazy=True)
-    def unicode():
-        return sa.text("SELECT 'λ' as a")
-
-    with Flow("flow") as f:
-        with Stage("stage"):
-            x = unicode()
-            x2 = m.noop(x)
-            x3 = m.noop_lazy(x2)
-            m.assert_table_equal(x, x2)
-            m.assert_table_equal(x, x3)
-
-    with StageLockContext():
-        result = f.run()
-        assert result.successful
-        assert result.get(x3, as_type=pd.DataFrame)["a"][0] == "λ"
+    test_unicode("λ")
