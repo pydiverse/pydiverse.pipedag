acb91602acdb3a740166ede2b1cabb981d28443f
Dominik Zuercher <dominikzuercher1999@gmail.com>
2024-03-15 08:19:59 +0100

    Adding max_nchars_query_print option for MSSqlTableStore

--- src/pydiverse/pipedag/backend/table/sql/dialects/mssql.py
+++ src/pydiverse/pipedag/backend/table/sql/dialects/mssql.py
@@ -51,6 +51,10 @@ class MSSqlTableStore(SQLTableStore):
         reach across, but it is not guaranteed to be identical to scripts executed
         by a SQL UI.
 
+    :param max_nchars_query_print:
+        Maximum number of characters in a SQL query that are printed when
+        logging the query before the log is truncated. Defaults to 5000 characters.
+
     .. _Microsoft SQL Server:
         https://www.microsoft.com/en/sql-server/
     .. _pytsql.executes:
@@ -64,10 +68,12 @@ class MSSqlTableStore(SQLTableStore):
         *args,
         disable_pytsql: bool = False,
         pytsql_isolate_top_level_statements: bool = True,
+        max_nchars_query_print: int = 5000,
         **kwargs,
     ):
         self.disable_pytsql = disable_pytsql
         self.pytsql_isolate_top_level_statements = pytsql_isolate_top_level_statements
+        self.max_nchars_query_print = max_nchars_query_print
 
         super().__init__(*args, **kwargs)
 
@@ -159,9 +165,10 @@ class MSSqlTableStore(SQLTableStore):
         sql_string = raw_sql.sql
         if self.print_sql:
             print_query_string = self.format_sql_string(sql_string)
-            max_length = 5000
-            if len(print_query_string) >= max_length:
-                print_query_string = print_query_string[:max_length] + " [...]"
+            if len(print_query_string) >= self.max_nchars_query_print:
+                print_query_string = (
+                    print_query_string[: self.max_nchars_query_print] + " [...]"
+                )
             self.logger.info("Executing sql", query=print_query_string)
         # ensure database connection is reset to original database
         sql_string += f"\nUSE {self.engine_url.database}"
