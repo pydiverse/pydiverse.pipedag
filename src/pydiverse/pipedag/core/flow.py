08cb00d6ade2295136a9872878de81a4459f076a
DominikZuercherQC <dominik.zuercher@quantco.com>
2024-07-17 17:28:18 +0200

    added warning

--- src/pydiverse/pipedag/core/flow.py
+++ src/pydiverse/pipedag/core/flow.py
@@ -296,8 +296,13 @@ class Flow:
         :param ignore_position_hashes:
             If ``True``, the position hashes of tasks are not checked
             when retrieving the inputs of a task from the cache.
-            This simplifies execution of subgraphs if you don't care whether inputs to that subgraph are cache invalid. This allows multiple modifications in the Graph before the next run updating the cache.
-            Attention: This may break automatic cache invalidation. And for this to work, any task producing an input for the chosen subgraph may never be used more than once per stage.
+            This simplifies execution of subgraphs if you don't care whether inputs to
+            that subgraph are cache invalid. This allows multiple modifications in the
+            Graph before the next run updating the cache.
+            Attention: This may break automatic cache invalidation.
+            And for this to work, any task producing an input
+            for the chosen subgraph may never be used more
+            than once per stage.
         :param kwargs:
             Other keyword arguments that get passed on directly to the
             ``run()`` method of the orchestration engine. Consequently, these
@@ -328,7 +333,12 @@ class Flow:
             flow.run(task_1, task_4)
 
         """
-
+        self.logger.warn(
+            "Using ignore_position_hashes=True! "
+            "This may break automatic cache invalidation. "
+            "And for this to work, any task producing an "
+            "input for the chosen subgraph may never be used more than once per stage."
+        )
         subflow = self.get_subflow(*components)
 
         # Get the ConfigContext to use
