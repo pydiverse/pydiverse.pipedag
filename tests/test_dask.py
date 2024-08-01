6de1fcfd5f950fe36df5033ff78885f16a79efe3
windiana42 <windiana@users.sf.net>
2024-08-01 14:40:18 +0200

    Fix dask tests by using python 3.11.
    Dask tests fail for 3.12 because pytest captures I/O and in python 3.12 the _pytest.capture.EncodedFile class cannot be made picklable by dask_patch.py adding __getstate__ and __setstate__ methods. See test_dask.py to analyze underlying problem.

---
+++ tests/test_dask.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+import io
+import pickle
+import sys
+from io import BytesIO
+
+import dask
+import pytest
+import structlog
+from _pytest.capture import EncodedFile
+
+# Attention: Running dask tests within pytest (without option -s) fails on python 3.12
+# because dask_patch.py cannot successfully make EncodedFile pickleable by adding
+# __getstate__ and __setstate__ methods. Let's see what the future of python brings.
+
+
+class A(io.TextIOWrapper):
+    def __getstate__(self):
+        return "a"
+
+    def __setstate__(self, x):
+        pass
+
+
+def test_that_io_wrapper_is_pickleable():
+    if sys.version_info >= (3, 12):
+        with pytest.raises(TypeError, match="cannot pickle 'A' instances"):
+            pickle.dumps(A(BytesIO(b"hello")))
+    else:
+        pickle.dumps(A(BytesIO(b"hello")))
+
+
+def test_that_encoded_file_is_picklable():
+    if sys.version_info >= (3, 12):
+        with pytest.raises(TypeError, match="cannot pickle 'EncodedFile' instances"):
+            pickle.dumps(EncodedFile(BytesIO(b"hello"), "utf-8"))
+    else:
+        pickle.dumps(EncodedFile(BytesIO(b"hello"), "utf-8"))
+
+
+def test_dask_structlog_configuration_does_not_prevent_pickling():
+    def bind_run():
+        structlog_config = structlog.get_config()
+
+        def run(parent_futures, **kwargs):
+            _ = parent_futures
+
+            structlog.configure(**structlog_config)
+
+            return 1
+
+        run.__name__ = "hi"
+        return dask.delayed(run, pure=False)
+
+    results = [bind_run()(parent_futures=[])]
+    kw = {
+        "traverse": True,
+        "optimize_graph": False,
+        "scheduler": "processes",
+        "num_workers": 8,
+        "chunksize": 1,
+    }
+
+    if sys.version_info >= (3, 12):
+        with pytest.raises(TypeError, match="cannot pickle 'EncodedFile' instances"):
+            dask.compute(results, **kw)
+    else:
+        dask.compute(results, **kw)
