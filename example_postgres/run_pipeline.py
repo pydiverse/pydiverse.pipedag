a13ac93a9cb7738cedf9def71c762b2d9e59da71
windiana42 <windiana@users.sf.net>
2025-07-29 14:35:26 +0200

    Extended postgres example with pydiverse.transform, dataframely, and colspec functionality

--- example_postgres/run_pipeline.py
+++ example_postgres/run_pipeline.py
@@ -1,24 +1,31 @@
 # Copyright (c) QuantCo and pydiverse contributors 2025-2025
 # SPDX-License-Identifier: BSD-3-Clause
-
+import dataframely as dy
 import pandas as pd
+import polars as pl
 import sqlalchemy as sa
 
+import pydiverse.colspec as cs
+import pydiverse.transform as pdt
 from pydiverse.common.util.structlog import setup_logging
-from pydiverse.pipedag import Flow, Stage, Table, materialize
+from pydiverse.pipedag import AUTO_VERSION, Flow, Stage, Table, materialize
 from pydiverse.pipedag.context import StageLockContext
+from pydiverse.transform.extended import left_join
 
 
 @materialize(lazy=True)
 def lazy_task_1():
-    return sa.select(
-        sa.literal(1).label("x"),
-        sa.literal(2).label("y"),
+    return Table(
+        sa.select(
+            sa.literal(1).label("x"),
+            sa.literal(2).label("y"),
+        ),
+        name="lazy_1",
     )
 
 
 @materialize(lazy=True, input_type=sa.Table)
-def lazy_task_2(input1: sa.sql.expression.Alias, input2: sa.sql.expression.Alias):
+def lazy_task_2(input1: sa.Alias, input2: sa.Alias):
     query = sa.select(
         (input1.c.x * 5).label("x5"),
         input2.c.a,
@@ -28,12 +35,12 @@ def lazy_task_2(input1: sa.sql.expression.Alias, input2: sa.sql.expression.Alias
 
 
 @materialize(lazy=True, input_type=sa.Table)
-def lazy_task_3(input1: sa.sql.expression.Alias):
+def lazy_task_3(input1: sa.Alias):
     return sa.text(f"SELECT * FROM {input1.original.schema}.{input1.original.name}")
 
 
 @materialize(lazy=True, input_type=sa.Table)
-def lazy_task_4(input1: sa.sql.expression.Alias):
+def lazy_task_4(input1: sa.Alias):
     return sa.text(f"SELECT * FROM {input1.original.schema}.{input1.original.name}")
 
 
@@ -59,6 +66,80 @@ def eager_task(tbl1: pd.DataFrame, tbl2: pd.DataFrame):
     return tbl1.merge(tbl2, on="x")
 
 
+@materialize(version=AUTO_VERSION, input_type=pl.LazyFrame)
+def eager_task_polars(tbl1: pl.LazyFrame, tbl2: pl.LazyFrame):
+    return tbl1.join(tbl2, on="x")
+
+
+@materialize(version=AUTO_VERSION, input_type=pdt.Polars)
+def eager_task_pdt(tbl1: pdt.Table, tbl2: pdt.Table):
+    return tbl1 >> left_join(tbl2, tbl1.x == tbl2.x) >> pdt.alias("eager_task_pdt_out")
+
+
+@materialize(lazy=True, input_type=pdt.SqlAlchemy)
+def lazy_task_pdt(tbl1: pdt.Table, tbl2: pdt.Table):
+    # pydiverse.transform syntax works both on Polars and SQL/duckdb backends.
+    return tbl1 >> left_join(tbl2, tbl1.x == tbl2.x) >> pdt.alias("lazy_task_pdt_out")
+
+
+class Tbl1Schema(dy.Schema):
+    x = dy.Int32()
+    y = dy.Int32()
+
+
+class OutputSchema(Tbl1Schema):
+    a = dy.Int64()
+
+
+@materialize(version=AUTO_VERSION, input_type=pl.LazyFrame)
+def eager_task_dataframely(tbl1: dy.LazyFrame[Tbl1Schema], tbl2: pl.LazyFrame) -> OutputSchema:
+    # Pipedag automatically calls Tbl1Schema.cast() and OutputSchema.validate().
+    return tbl1.join(tbl2, on="x")
+
+
+@materialize(version=AUTO_VERSION, input_type=pdt.Polars)
+def eager_task_dataframely_pdt(tbl1: Tbl1Schema, tbl2: pdt.Table) -> OutputSchema:
+    # Pipedag automatically calls Tbl1Schema.cast() and OutputSchema.validate().
+    return tbl1 >> left_join(tbl2, tbl1.x == tbl2.x) >> pdt.alias("eager_task_dataframely_pdt_out")
+
+
+class Tbl1ColSpec(cs.ColSpec):
+    x = cs.Int32()
+    y = cs.Int32()
+
+
+class OutputColSpec(Tbl1ColSpec):
+    a = cs.Int64()
+
+
+@materialize(version=AUTO_VERSION, input_type=pl.LazyFrame)
+def eager_task_colspec(tbl1: Tbl1ColSpec, tbl2: pl.LazyFrame) -> OutputColSpec:
+    # Pipedag automatically calls Tbl1ColSpec.cast_polars() and OutputColSpec.validate_polars()
+    # which call dataframely in the background. ColSpec classes can also be used for SQL validations.
+    return tbl1.join(tbl2, on="x")
+
+
+@materialize(version=AUTO_VERSION, input_type=pdt.Polars)
+def eager_task_colspec_pdt(tbl1: Tbl1ColSpec, tbl2: pdt.Table) -> OutputColSpec:
+    # Pipedag automatically calls Tbl1ColSpec.cast_polars() and OutputColSpec.validate_polars()
+    # which call dataframely in the background. ColSpec classes can also be used for SQL validations.
+    return tbl1 >> left_join(tbl2, tbl1.x == tbl2.x) >> pdt.alias("eager_task_colspec_pdt_out")
+
+
+@materialize(lazy=True, input_type=sa.Table)
+def lazy_task_colspec(tbl1: Tbl1ColSpec, tbl2: sa.Alias) -> OutputColSpec:
+    # Pipedag automatically calls Tbl1ColSpec.cast() and OutputColSpec.validate()
+    return Table(
+        sa.select(tbl1, tbl2.c.a).select_from(tbl1.outerjoin(tbl2, tbl1.c.x == tbl2.c.x)), name="lazy_task_colspec"
+    )
+
+
+@materialize(lazy=True, input_type=pdt.SqlAlchemy)
+def lazy_task_colspec_pdt(tbl1: Tbl1ColSpec, tbl2: pdt.Table) -> OutputColSpec:
+    # Pipedag automatically calls Tbl1ColSpec.cast() and OutputColSpec.validate()
+    return tbl1 >> left_join(tbl2, tbl1.x == tbl2.x) >> pdt.alias("lazy_task_colspec_pdt_out")
+
+
 def main():
     with Flow() as f:
         with Stage("stage_1"):
@@ -68,11 +149,21 @@ def main():
         with Stage("stage_2"):
             lazy_2 = lazy_task_2(lazy_1, b)
             lazy_3 = lazy_task_3(lazy_2)
-            eager = eager_task(lazy_1, b)
+            # Do the same operation with various syntax alternatives.
+            # This is also a demonstration how pipedag interacts with \
+            # dataframely, pydiverse.colspec, and pydiverse.transform.
+            out = eager_task(lazy_1, b)
+            out2 = eager_task_polars(lazy_1, b)
+            out3 = eager_task_pdt(lazy_1, b)
+            out4 = lazy_task_pdt(lazy_1, b)
+            out5 = eager_task_dataframely(lazy_1, b)
+            out6 = eager_task_dataframely_pdt(lazy_1, b)
+            out7 = eager_task_colspec(lazy_1, b)
+            out8 = lazy_task_colspec(lazy_1, b)
 
         with Stage("stage_3"):
             lazy_4 = lazy_task_4(lazy_2)
-        _ = lazy_3, lazy_4, eager  # unused terminal output tables
+        _ = lazy_3, lazy_4, out, out2, out3, out4, out5, out6, out7, out8  # unused terminal output tables
 
     # Run flow
     result = f.run()
@@ -93,7 +184,7 @@ if __name__ == "__main__":
     # pixi run python run_pipeline.py
     # ```
 
-    # don't forget to run docker-compose in separate shell to launch postgres container:
+    # don't forget to run docker-compose in separate shell to launch minio container:
     # ```shell
     # pixi run docker-compose up
     # ```
