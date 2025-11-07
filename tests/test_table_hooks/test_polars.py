e20b3ba933c49b774d4ab2b42481c6866b28d83f
windiana42 <windiana@users.sf.net>
2025-11-07 08:01:05 +0100

    Speedup snowflake tests 3min.

--- tests/test_table_hooks/test_polars.py
+++ tests/test_table_hooks/test_polars.py
@@ -96,6 +96,7 @@ def test_table_store():
     assert f.run().successful
 
 
+@skip_instances("snowflake")  # speedup tests 3min
 def test_auto_version_1(mocker):
     should_swap = True
     value_to_add = 1
